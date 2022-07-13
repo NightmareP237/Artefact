d56af2a2917cd29fe1c51561d5ebd94d7bd1dc50
Frank Armel <92791908+NightmareP237@users.noreply.github.com>
2022-07-13 11:43:44 +0200

    Create text_field_container.dart

---
+++ lib/components/text_field_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:artefact/constants.dart';
+
+class TextFieldContainer extends StatefulWidget {
+  final Widget? child;
+  const TextFieldContainer({
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  State<TextFieldContainer> createState() => _TextFieldContainerState();
+}
+
+class _TextFieldContainerState extends State<TextFieldContainer> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kPrimaryLightColor,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: widget.child,
+    );
+  }
+}
