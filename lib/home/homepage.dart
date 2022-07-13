84f2749cda4266e25a1adfb67778cc55a44dec52
Frank Armel <92791908+NightmareP237@users.noreply.github.com>
2022-07-13 11:43:52 +0200

    Create homepage.dart

---
+++ lib/home/homepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage("gif/nofile.gif")),
+            Text("Aucun document disponible "),
+          ],
+        ),
+      ),
+    );
+  }
+}
